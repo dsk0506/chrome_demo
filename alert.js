1c5a4100f2f3cb8be5eb5adfea4398f686aa4f86
dingshoukun <dingshoukun@quancheng-ec.com>
2016-07-15 18:11:03 +0800

    chrome 扩展demo

--- alert.js
+++ alert.js
@@ -1,8 +1,5 @@
 $("#login-username").val("18616369917");
-$("#login-username").click();
-$("#login-password").focus();
 $("#login-password").val("123123");
-setTimeout(function () {
-    $("button[type='submit']").click();
+$("button[type='submit']").click();
 
-},200)
+
--- popup.js
+++ popup.js
@@ -1,8 +1,44 @@
 function hello() {
     chrome.tabs.executeScript(null, {file: "jquery-3.0.0.min.js"}, function () {
-        chrome.tabs.executeScript(null, {file: "alert.js"});
+            chrome.tabs.executeScript(null, {file: "alert.js"});
     });
 
 }
 
-document.getElementById('clickme').addEventListener('click', hello);
+
+document.getElementById('web').addEventListener('click', function () {
+    window.open("http://www.qccost.com");
+});
+document.getElementById('testtools').addEventListener('click', function () {
+    window.open("http://php.fk.com/tools/index");
+});
+document.getElementById('testme').addEventListener('click', function () {
+    window.open("http://php.fk.com/");
+});
+document.getElementById('qa1tools').addEventListener('click', function () {
+    window.open("http://api.fk1.qa/tools/index");
+});
+document.getElementById('qa1me').addEventListener('click', function () {
+    window.open("http://me.fk1.qa");
+});
+document.getElementById('qa2tools').addEventListener('click', function () {
+    window.open("http://api.fk2.qa/tools/index");
+});
+document.getElementById('qa2me').addEventListener('click', function () {
+    window.open("http://me.fk2.qa");
+});
+document.getElementById('preme').addEventListener('click', function () {
+    window.open("http://me.fk.pre");
+});
+document.getElementById('gitlab').addEventListener('click', function () {
+    window.open("http://gitlab.qc.com");
+});
+document.getElementById('tower').addEventListener('click', function () {
+    window.open("http://tower.im/");
+});
+document.getElementById('redmine').addEventListener('click', function () {
+    window.open("http://redmine.qc.com");
+});
+document.getElementById('jenkins').addEventListener('click', function () {
+    window.open("http://ijenkins.qc.com");
+});
